4aaa90681dee741687cae28f70accd3261ed20bc
Roberto Tatasciore <tatasciorecont@gmail.com>
2020-08-30 15:24:38 +0200

    Import clientId instead of hard-coding it

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,15 +1,10 @@
 import React, { useEffect } from 'react';
 import WebView from 'react-native-webview';
 import { apiService } from '../../api/ApiService';
+import { clientId } from '../../config';
 
 export const LoginScreen = () => {
-  const state = 12345;
-
-  const response =
-    'access_token=6e4e7d11b3eb16e9d289e3420b287b6da6fe2dc2&scope=&token_type=bearer';
-
-  const token = '6e4e7d11b3eb16e9d289e3420b287b6da6fe2dc2'; //response.split('&')[0].split('=')[1];
-  console.log('Token', token);
+  const state = '12345';
 
   useEffect(() => {
     apiService
@@ -23,7 +18,7 @@ export const LoginScreen = () => {
   return (
     <WebView
       source={{
-        uri: `https://github.com/login/oauth/authorize?client_id=1636907eaa1bdd669203&state=${state}`,
+        uri: `https://github.com/login/oauth/authorize?client_id=${clientId}&state=${state}`,
       }}
       onNavigationStateChange={e => {
         const { url } = e;
